b47f8ce599e58812a384fe7f7820cc6c7b9cec42
Anna Morris <annapatton444@gmail.com>
2021-01-29 20:04:28 -0600

    Form sets state and has default values

--- topia-frontend-challenge-master/src/App.js
+++ topia-frontend-challenge-master/src/App.js
@@ -1,9 +1,45 @@
 import "./App.css";
+import React, { useState } from 'react';
 import { UserList } from "./components/UserList";
 
 function App() {
+  const [coordinates, setCoordinates] = useState({
+    x: 800,
+    y: 400
+  })
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(coordinates)
+  };
+
+  const handleChanges = (event) => {
+    const newCoord = event.target.name;
+    setCoordinates({
+      ...coordinates,
+      [newCoord]: event.target.value
+    })
+  };
+
   return (
     <div className="App">
+      <form>My Coordinates
+      <lable>X</lable>
+        <input
+          name="x"
+          type="number"
+          defaultValue={coordinates.x}
+          onChange={handleChanges}>
+        </input>
+        <lable>Y</lable>
+        <input
+          name="y"
+          type="number"
+          defaultValue={coordinates.y}
+          onChange={handleChanges}
+        ></input>
+        <button onClick={handleSubmit}>User List</button>
+      </form>
       <UserList />
     </div>
   );
