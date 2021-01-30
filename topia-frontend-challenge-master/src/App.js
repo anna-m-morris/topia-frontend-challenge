831c82cdee7621af08f18d21786315b9eb9143bc
Anna Morris <annapatton444@gmail.com>
2021-01-29 21:30:51 -0600

    prep for new instuctions

--- topia-frontend-challenge-master/src/App.js
+++ topia-frontend-challenge-master/src/App.js
@@ -7,10 +7,11 @@ function App() {
     x: 800,
     y: 400
   })
+  const [modal, setModal] = useState(false)
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
+  const showModal = (event) => {
     console.log(coordinates)
+    setModal(true)
   };
 
   const handleChanges = (event) => {
@@ -24,23 +25,23 @@ function App() {
   return (
     <div className="App">
       <form>My Coordinates
-      <lable>X</lable>
+      <label>X</label>
         <input
           name="x"
           type="number"
           defaultValue={coordinates.x}
           onChange={handleChanges}>
         </input>
-        <lable>Y</lable>
+        <label>Y</label>
         <input
           name="y"
           type="number"
           defaultValue={coordinates.y}
           onChange={handleChanges}
         ></input>
-        <button onClick={handleSubmit}>User List</button>
       </form>
-      <UserList />
+        <button onClick={showModal}>User List</button>
+      <UserList show={modal} />
     </div>
   );
 }
